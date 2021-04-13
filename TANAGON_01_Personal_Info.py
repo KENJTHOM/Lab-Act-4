5cdc2893cef5e1f93de313a72eceaad85704693d
KENJTHOM <82429410+KENJTHOM@users.noreply.github.com>
2021-04-13 12:21:24 +0800

    Add files via upload

---
+++ TANAGON_01_Personal_Info.py
@@ -0,0 +1,6 @@
+fName = input("Please Enter your First Name: ")
+lName = input("Please Enter your Last Name: ")
+loc = input("Please Enter your Location: ")
+age = int(input("Please Enter your Age: "))
+space = " "
+print("Hola! Your name is " +fName +space +lName +space +str(age) +"years old." +space +"And you live in" +space +loc)
